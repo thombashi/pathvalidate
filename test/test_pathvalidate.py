a162e6cb46a28f6fe03adf59f065535d83d2b4b9
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-05-22 16:13:09 +0900

    Update tests

--- test/test_pathvalidate.py
+++ test/test_pathvalidate.py
@@ -1,9 +1,8 @@
 # encoding: utf-8
 
-'''
-@author: Tsuyoshi Hombashi
-'''
-
+"""
+.. codeauthor:: Tsuyoshi Hombashi <gogogo.vm@gmail.com>
+"""
 
 import itertools
 import random
@@ -18,20 +17,28 @@ random.seed(0)
 
 char_list = [x for x in string.digits + string.ascii_letters + "/"]
 
+INVALID_PATH_CHARS = [
+    "\\", ":", "*", "?", '"', "<", ">", "|",
+]
+INVALID_VAR_CHARS = INVALID_PATH_CHARS + [
+    "!", "#", "$", '&', "'",
+    "=", "~", "^", "@", "`", "[", "]", "+", "-", ";", "{", "}",
+    ",", ".", "(", ")", "%",
+]
+VALID_PATH_CHARS = [
+    "!", "#", "$", '&', "'", "_",
+    "=", "~", "^", "@", "`", "[", "]", "+", "-", ";", "{", "}",
+    ",", ".", "(", ")", "%",
+]
+
 
 def make_random_str(length):
     return "".join([random.choice(char_list) for _i in range(length)])
 
 
 class Test_validate_filename:
-    VALID_CHAR_LIST = [
-        "!", "#", "$", '&', "'", "_",
-        "=", "~", "^", "@", "`", "[", "]", "+", "-", ";", "{", "}",
-        ",", ".", "(", ")", "%",
-    ]
-    INVALID_CHAR_LIST = [
-        "\\", ":", "*", "?", '"', "<", ">", "|",
-    ]
+    VALID_CHAR_LIST = VALID_PATH_CHARS
+    INVALID_CHAR_LIST = INVALID_PATH_CHARS
 
     @pytest.mark.parametrize(["value"], [
         [make_random_str(64) + invalid_char + make_random_str(64)]
@@ -58,9 +65,48 @@ class Test_validate_filename:
             validate_filename(value)
 
 
+class Test_validate_python_var_name:
+    VALID_CHAR_LIST = [
+        c for c in string.digits + string.ascii_letters + "_"
+    ]
+    INVALID_CHAR_LIST = INVALID_PATH_CHARS
+
+    @pytest.mark.parametrize(["value"], [
+        ["abc" + valid_char + "hoge123"]
+        for valid_char in VALID_CHAR_LIST
+    ])
+    def test_normal(self, value):
+        validate_python_var_name(value)
+
+    @pytest.mark.parametrize(["value"], [
+        ["abc" + invalid_char + "hoge123"]
+        for invalid_char in INVALID_CHAR_LIST
+    ])
+    def test_exception_0(self, value):
+        with pytest.raises(ValueError):
+            validate_python_var_name(value)
+
+    @pytest.mark.parametrize(["value"], [
+        [invalid_char + "hoge123"]
+        for invalid_char in [string.digits + "_"]
+    ])
+    def test_exception_1(self, value):
+        with pytest.raises(ValueError):
+            validate_python_var_name(value)
+
+    @pytest.mark.parametrize(["value", "expected"], [
+        [None, ValueError],
+        [1, ValueError],
+        [True, ValueError],
+    ])
+    def test_exception_2(self, value, expected):
+        with pytest.raises(expected):
+            validate_python_var_name(value)
+
+
 class Test_sanitize_filename:
-    SANITIZE_CHAR_LIST = Test_validate_filename.INVALID_CHAR_LIST
-    NOT_SANITIZE_CHAR_LIST = Test_validate_filename.VALID_CHAR_LIST
+    SANITIZE_CHAR_LIST = INVALID_PATH_CHARS
+    NOT_SANITIZE_CHAR_LIST = VALID_PATH_CHARS
     REPLACE_TEXT_LIST = ["", "_"]
 
     @pytest.mark.parametrize(
@@ -76,7 +122,9 @@ class Test_sanitize_filename:
         ]
     )
     def test_normal(self, value, replace_text, expected):
-        assert sanitize_filename(value, replace_text) == expected
+        sanitized_name = sanitize_filename(value, replace_text)
+        assert sanitized_name == expected
+        validate_filename(sanitized_name)
 
     @pytest.mark.parametrize(["value", "expected"], [
         [None, AttributeError],
@@ -89,14 +137,7 @@ class Test_sanitize_filename:
 
 
 class Test_sanitize_python_var_name:
-    SANITIZE_CHAR_LIST = (
-        Test_validate_filename.INVALID_CHAR_LIST +
-        [
-            "!", "#", "$", '&', "'",
-            "=", "~", "^", "@", "`", "[", "]", "+", "-", ";", "{", "}",
-            ",", ".", "(", ")", "%",
-        ]
-    )
+    SANITIZE_CHAR_LIST = INVALID_VAR_CHARS
     NOT_SANITIZE_CHAR_LIST = ["_"]
     REPLACE_TEXT_LIST = ["", "_"]
 
@@ -113,14 +154,24 @@ class Test_sanitize_python_var_name:
         ]
     )
     def test_normal(self, value, replace_text, expected):
-        assert sanitize_python_var_name(value, replace_text) == expected
+        sanitized_name = sanitize_python_var_name(value, replace_text)
+        assert sanitized_name == expected
+        validate_python_var_name(sanitized_name)
+
+    @pytest.mark.parametrize(["value"], [
+        [invalid_char + "hoge123"]
+        for invalid_char in [string.digits + "_"]
+    ])
+    def test_exception_0(self, value):
+        with pytest.raises(ValueError):
+            sanitize_python_var_name(value)
 
     @pytest.mark.parametrize(["value", "expected"], [
         [None, AttributeError],
         [1, AttributeError],
         [True, AttributeError],
     ])
-    def test_exception(self, value, expected):
+    def test_exception_1(self, value, expected):
         with pytest.raises(expected):
             sanitize_python_var_name(value)
 
