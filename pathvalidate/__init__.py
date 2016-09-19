3683d00fd95c6d1836e0171595075eccb6b6b65c
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-09-19 19:58:13 +0900

    Add imports

--- pathvalidate/__init__.py
+++ pathvalidate/__init__.py
@@ -7,10 +7,12 @@
 from __future__ import absolute_import
 
 from ._error import NullNameError
+from ._error import InvalidNameError
 from ._error import InvalidCharError
 from ._error import InvalidCharWindowsError
 from ._error import InvalidLengthError
 from ._error import ReservedNameError
+from ._error import ValidReservedNameError
 
 from ._app import validate_excel_sheet_name
 from ._app import sanitize_excel_sheet_name
