3f520dff05f9d15d12a6bfa662a3ec002dbc0d8b
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-23 19:33:51 +0900

    Modify imports declarations

--- pathvalidate/__init__.py
+++ pathvalidate/__init__.py
@@ -17,15 +17,25 @@ from ._error import (
     InvalidReservedNameError
 )
 
-from ._app import validate_excel_sheet_name
-from ._app import sanitize_excel_sheet_name
-from ._file import validate_filename
-from ._file import validate_file_path
-from ._file import sanitize_filename
-from ._file import sanitize_file_path
-from ._symbol import validate_symbol
-from ._symbol import replace_symbol
-from ._sqlite import validate_sqlite_table_name
-from ._sqlite import validate_sqlite_attr_name
-from ._var_name import validate_python_var_name
-from ._var_name import sanitize_python_var_name
+from ._app import (
+    validate_excel_sheet_name,
+    sanitize_excel_sheet_name
+)
+from ._file import (
+    validate_filename,
+    validate_file_path,
+    sanitize_filename,
+    sanitize_file_path
+)
+from ._symbol import (
+    validate_symbol,
+    replace_symbol
+)
+from ._sqlite import (
+    validate_sqlite_table_name,
+    validate_sqlite_attr_name
+)
+from ._var_name import (
+    validate_python_var_name,
+    sanitize_python_var_name
+)
