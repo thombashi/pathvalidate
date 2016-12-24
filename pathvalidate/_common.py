186567fdc127e5c08131ebbf49b76a7f7430de6a
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-24 17:00:36 +0900

    Change to modifiable error message

--- pathvalidate/_common.py
+++ pathvalidate/_common.py
@@ -12,6 +12,6 @@ import dataproperty
 from ._error import NullNameError
 
 
-def _validate_null_string(text):
+def _validate_null_string(text, error_msg="null name"):
     if dataproperty.is_empty_string(text):
-        raise NullNameError("null name")
+        raise NullNameError(error_msg)
