6af143ab800b98b5ac215db0eb934b122ae860c8
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-28 14:32:01 +0900

    Add an argument to file name/path validate method
    The argument represents execution platform.

--- pathvalidate/_file.py
+++ pathvalidate/_file.py
@@ -8,6 +8,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 import itertools
 import os.path
+import platform
 import re
 
 from mbstrdecoder import MultiByteStrDecoder
@@ -49,12 +50,30 @@ __WINDOWS_RESERVED_FILE_NAME_LIST = [
 __MAX_FILENAME = 255
 __LINUX_MAX_PATH = 1024
 
+__VALID_WIN_PLATFORM_NAME_LIST = ["windows", "win"]
 
-def validate_filename(filename):
+__error_message_template = "invalid char found : invalid-char='{}', value='{}'"
+
+
+def __validate_win_filename(unicode_filename):
+    match = __RE_INVALID_WIN_FILENAME.search(unicode_filename)
+    if match is not None:
+        raise InvalidCharWindowsError(__error_message_template.format(
+            unicode_filename, re.escape(match.group())))
+
+    if unicode_filename.upper() in __WINDOWS_RESERVED_FILE_NAME_LIST:
+        raise InvalidReservedNameError(
+            "{} is a reserved name by Windows".format(unicode_filename))
+
+
+def validate_filename(filename, platform_name=None):
     """
     Verifying whether the ``filename`` is a valid file name or not.
 
     :param str filename: Filename to validate.
+    :param str platform_name:
+        Execution platform. Available names are ``"Linux"`` or ``"Windows"``.
+        Defaults to |None| (automatically detect platform).
     :raises pathvalidate.NullNameError: If the ``filename`` is empty.
     :raises pathvalidate.InvalidLengthError:
         If the ``filename`` is longer than 255 characters.
@@ -81,24 +100,28 @@ def validate_filename(filename):
                 __MAX_FILENAME, len(filename)))
 
     error_message_template = "invalid char found in the filename: '{:s}'"
-
     unicode_filename = MultiByteStrDecoder(filename).unicode_str
-    match = __RE_INVALID_FILENAME.search(unicode_filename)
+
+    if platform_name is None:
+        platform_name = platform.system()
+
+    if platform_name.lower() in __VALID_WIN_PLATFORM_NAME_LIST:
+        __validate_win_filename(unicode_filename)
+    else:
+        match = __RE_INVALID_FILENAME.search(unicode_filename)
+        if match is not None:
+            raise InvalidCharError(
+                error_message_template.format(re.escape(match.group())))
+
+
+def __validate_win_file_path(unicode_file_path):
+    match = __RE_INVALID_WIN_PATH.search(unicode_file_path)
     if match is not None:
-        raise InvalidCharError(
-            error_message_template.format(re.escape(match.group())))
-
-    match = __RE_INVALID_WIN_FILENAME.search(unicode_filename)
-    if match is not None:
-        raise InvalidCharWindowsError(
-            error_message_template.format(re.escape(match.group())))
-
-    if unicode_filename.upper() in __WINDOWS_RESERVED_FILE_NAME_LIST:
-        raise InvalidReservedNameError(
-            "{} is a reserved name by Windows".format(unicode_filename))
+        raise InvalidCharWindowsError(__error_message_template.format(
+            re.escape(match.group()), unicode_file_path))
 
 
-def validate_file_path(file_path):
+def validate_file_path(file_path, platform_name=None):
     """
     Verifying whether the ``file_path`` is a valid file path or not.
 
@@ -121,20 +144,19 @@ def validate_file_path(file_path):
 
     _validate_null_string(file_path)
 
-    error_message_template = "invalid char found : invalid-char='{}', path='{}'"
-
     file_path = os.path.normpath(os.path.splitdrive(file_path)[1])
-
     unicode_file_path = MultiByteStrDecoder(file_path).unicode_str
-    match = __RE_INVALID_PATH.search(unicode_file_path)
-    if match is not None:
-        raise InvalidCharError(error_message_template.format(
-            re.escape(match.group()), unicode_file_path))
 
-    match = __RE_INVALID_WIN_PATH.search(unicode_file_path)
-    if match is not None:
-        raise InvalidCharWindowsError(error_message_template.format(
-            re.escape(match.group()), unicode_file_path))
+    if platform_name is None:
+        platform_name = platform.system()
+
+    if platform_name.lower() in __VALID_WIN_PLATFORM_NAME_LIST:
+        __validate_win_file_path(unicode_file_path)
+    else:
+        match = __RE_INVALID_PATH.search(unicode_file_path)
+        if match is not None:
+            raise InvalidCharError(__error_message_template.format(
+                re.escape(match.group()), unicode_file_path))
 
     if len(unicode_file_path) > __LINUX_MAX_PATH:
         raise InvalidLengthError(
@@ -144,9 +166,10 @@ def validate_file_path(file_path):
 
 def sanitize_filename(filename, replacement_text=""):
     """
+    Make valid filename for both Windows and Linux.
     Replace invalid characters for a filename within the ``filename``
     with the ``replacement_text``. Invalid characters are as follows:
-    |invalid_filename_chars|.
+    |invalid_filename_chars|, |invalid_win_filename_chars|.
 
     :param str filename: Filename to sanitize.
     :param str replacement_text: Replacement text.
@@ -168,7 +191,7 @@ def sanitize_file_path(file_path, replacement_text=""):
     """
     Replace invalid characters for a file path within the ``file_path``
     with the ``replacement_text``. Invalid characters are as follows:
-    |invalid_file_path_chars|.
+    |invalid_file_path_chars|, |invalid_win_file_path_chars|.
 
     :param str file_path: File path to sanitize.
     :param str replacement_text: Replacement text.
--- test/test_file.py
+++ test/test_file.py
@@ -27,6 +27,8 @@ from ._common import (
 
 random.seed(0)
 
+VALID_PLATFORM_NAME_LIST = ["linux", "windows"]
+
 WIN_RESERVED_FILE_NAME_LIST = [
     "CON", "con",
     "PRN", "prn",
@@ -38,9 +40,9 @@ WIN_RESERVED_FILE_NAME_LIST = [
     in itertools.product(["COM", "com", "LPT", "lpt"], range(1, 10))
 ]
 
-UTF8_NAME_LIST = [
-    ["あいうえお.txt".encode("utf_8")],
-    #["属性.txt".encode("utf_16")],
+VALID_MULTIBYTE_NAME_LIST = [
+    "新しいテキスト ドキュメント.txt".encode("utf_8"),
+    "新規 Microsoft Excel Worksheet.xlsx".encode("utf_16"),
 ]
 
 
@@ -48,46 +50,66 @@ class Test_validate_filename:
     VALID_CHAR_LIST = VALID_FILENAME_CHARS
     INVALID_CHAR_LIST = INVALID_WIN_FILENAME_CHARS
 
-    @pytest.mark.parametrize(["value"], [
-        [make_random_str(64) + invalid_char + make_random_str(64)]
-        for invalid_char in VALID_CHAR_LIST
-    ] + [
-        ["テスト.txt".encode("utf_8")],
-    ])
-    def test_normal(self, value):
-        validate_filename(value)
+    @pytest.mark.parametrize(
+        ["value", "platform_name"],
+        itertools.chain.from_iterable([
+            [
+                arg_list for arg_list in itertools.product(
+                    [make_random_str(64) + valid_char + make_random_str(64)],
+                    VALID_PLATFORM_NAME_LIST
+                )
+            ]
+            for valid_char in VALID_CHAR_LIST
+        ])
+    )
+    def test_normal(self, value, platform_name):
+        validate_filename(value, platform_name)
 
-    @pytest.mark.parametrize(["value"], [
-        ["あいうえお.txt".encode("utf_8")],
-        ["属性.txt".encode("utf_16")],
-    ])
-    def test_normal_multibyte(self, value):
-        validate_filename(value)
+    @pytest.mark.parametrize(
+        ["value", "platform_name"],
+        itertools.chain.from_iterable([
+            [
+                arg_list for arg_list in itertools.product(
+                    [multibyte_name], VALID_PLATFORM_NAME_LIST)
+            ]
+            for multibyte_name in VALID_MULTIBYTE_NAME_LIST
+        ])
+    )
+    def test_normal_multibyte(self, value, platform_name):
+        validate_filename(value, platform_name)
 
-    @pytest.mark.parametrize(["value"], [
-        [make_random_str(64) + invalid_char + make_random_str(64)]
-        for invalid_char in INVALID_FILENAME_CHARS
-    ])
-    def test_exception_invalid_char(self, value):
+    @pytest.mark.parametrize(
+        ["value", "platform_name"],
+        itertools.chain.from_iterable([
+            [
+                arg_list for arg_list in itertools.product(
+                    [make_random_str(64) + invalid_char + make_random_str(64)],
+                    VALID_PLATFORM_NAME_LIST
+                )
+            ]
+            for invalid_char in INVALID_FILENAME_CHARS
+        ])
+    )
+    def test_exception_invalid_char(self, value, platform_name):
         with pytest.raises(InvalidCharError):
-            validate_filename(value)
+            validate_filename(value, platform_name)
 
     @pytest.mark.parametrize(["value"], [
         [make_random_str(64) + invalid_char + make_random_str(64)]
         for invalid_char in set(INVALID_WIN_PATH_CHARS).difference(
             set(INVALID_PATH_CHARS + INVALID_FILENAME_CHARS))
     ])
-    def test_exception_invalid_win_char(self, value):
+    def test_exception_win_invalid_char(self, value):
         with pytest.raises(InvalidCharWindowsError):
-            validate_filename(value)
+            validate_filename(value, platform_name="windows")
 
     @pytest.mark.parametrize(["value", "expected"], [
         [reserved_keyword, InvalidReservedNameError]
         for reserved_keyword in WIN_RESERVED_FILE_NAME_LIST
     ])
-    def test_exception_reserved(self, value, expected):
+    def test_exception_win_reserved_name(self, value, expected):
         with pytest.raises(expected):
-            validate_filename(value)
+            validate_filename(value, platform_name="windows")
 
     @pytest.mark.parametrize(["value", "expected"], [
         [None, NullNameError],
@@ -103,20 +125,53 @@ class Test_validate_filename:
 
 class Test_validate_file_path:
     VALID_CHAR_LIST = VALID_PATH_CHARS
+    VALID_MULTIBYTE_PATH_LIST = [
+        "\\\\localhost\\Users\\新しいフォルダー\\あいうえお.txt".encode("utf_8"),
+        "\\\\localhost\\新しいフォルダー\\ユーザ属性.txt".encode("utf_16"),
+    ]
+    WIN_VALID_PATH_LIST = [
+        '\\\\localhost\\Users\\test\\AppData\\Local\\Temp\\pytest-of-test\\pytest-0\\test_exception__hoge_csv_heade1\\hoge.csv',
+        '\\\\localhost/Users/test/AppData/Local/Temp/pytest-of-test/pytest-0/test_exception__hoge_csv_heade1/hoge.csv',
+        '\\\\localhost\\Users\\test\\AppData/Local\\Temp/pytest-of-test\\pytest-0/test_exception__hoge_csv_heade1\\hoge.csv',
+        '\\\\localhost\\Users',
+        '\\\\localhost\\',
+        '\\Users',
+    ]
+
+    @pytest.mark.parametrize(
+        ["value", "platform_name"],
+        itertools.chain.from_iterable([
+            [
+                arg_list for arg_list in itertools.product(
+                    [make_random_str(64) + valid_char + make_random_str(64)],
+                    VALID_PLATFORM_NAME_LIST
+                )
+            ]
+            for valid_char in VALID_CHAR_LIST
+        ])
+    )
+    def test_normal(self, value, platform_name):
+        validate_file_path(value, platform_name)
+
+    @pytest.mark.parametrize(
+        ["value", "platform_name"],
+        itertools.chain.from_iterable([
+            [
+                arg_list for arg_list in itertools.product(
+                    [valid_path], VALID_PLATFORM_NAME_LIST)
+            ]
+            for valid_path in VALID_MULTIBYTE_PATH_LIST
+        ])
+    )
+    def test_normal_multibyte(self, value, platform_name):
+        validate_file_path(value, platform_name)
 
     @pytest.mark.parametrize(["value"], [
-        [make_random_str(64) + invalid_char + make_random_str(64)]
-        for invalid_char in VALID_CHAR_LIST
+        [valid_path]
+        for valid_path in WIN_VALID_PATH_LIST
     ])
-    def test_normal(self, value):
-        validate_file_path(value)
-
-    @pytest.mark.parametrize(["value"], [
-        ["/tmp/ユーザ/あいうえお.txt".encode("utf_8")],
-        ["/tmp/ユーザ属性.txt".encode("utf_16")],
-    ])
-    def test_normal_multibyte(self, value):
-        validate_file_path(value)
+    def test_normal_win(self, value):
+        validate_file_path(value, "windows")
 
     @pytest.mark.parametrize(["value"], [
         [make_random_str(64) + invalid_char + make_random_str(64)]
@@ -133,7 +188,7 @@ class Test_validate_file_path:
     ])
     def test_exception_invalid_win_char(self, value):
         with pytest.raises(InvalidCharWindowsError):
-            validate_file_path(value)
+            validate_file_path(value, platform_name="windows")
 
     @pytest.mark.parametrize(["value", "expected"], [
         [None, NullNameError],
