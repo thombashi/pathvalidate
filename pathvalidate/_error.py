4c7e80fee375cb7391c9afc4f0a8ea84b53af45a
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-12-31 10:27:30 +0900

    [ci skip] Update docstrings

--- pathvalidate/_error.py
+++ pathvalidate/_error.py
@@ -10,49 +10,49 @@ from __future__ import unicode_literals
 
 class InvalidNameError(ValueError):
     """
-    Base class of invalid name error.
+    Base exception class that indicates invalid name errors.
     """
 
 
 class NullNameError(InvalidNameError):
     """
-    Raised when a name is empty.
+    Exception raised when a name is empty.
     """
 
 
 class InvalidCharError(InvalidNameError):
     """
-    Raised when includes invalid character(s) within a string.
+    Exception raised when includes invalid character(s) within a string.
     """
 
 
 class InvalidCharWindowsError(InvalidCharError):
     """
-    Raised when includes Windows specific invalid character(s) within a string.
+    Exception raised when includes Windows specific invalid character(s) within a string.
     """
 
 
 class InvalidLengthError(InvalidNameError):
     """
-    Raised when a string too long/short.
+    Exception raised when a string too long/short.
     """
 
 
 class ReservedNameError(InvalidNameError):
     """
-    Raised when a string is matched a reserved name.
+    Exception raised when a string is matched a reserved name.
     """
 
 
 class ValidReservedNameError(ReservedNameError):
     """
-    Raised when a string is matched a reserved name.
+    Exception raised when a string is matched a reserved name.
     However, it can be used as a name.
     """
 
 
 class InvalidReservedNameError(ReservedNameError):
     """
-    Raised when a string is matched a reserved name.
+    Exception raised when a string is matched a reserved name.
     And the reserved name is invalid as a name.
     """
