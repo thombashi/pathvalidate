bdd7cdfa4c484a031d540f61cdc461d94033dcdd
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-02-18 20:15:38 +0900

    Bump version to 0.16.3

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 
 setuptools.setup(
     name="pathvalidate",
-    version="0.16.2",
+    version="0.16.3",
     author="Tsuyoshi Hombashi",
     author_email="tsuyoshi.hombashi@gmail.com",
     url="https://github.com/thombashi/pathvalidate",
@@ -62,5 +62,4 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries",
         "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-)
+    ])
