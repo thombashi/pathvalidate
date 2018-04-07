a310f121c43e3f08202cf949c37c676930a3d3ea
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-07 10:25:15 +0900

    Apply isort

--- pathvalidate/__init__.py
+++ pathvalidate/__init__.py
@@ -6,44 +6,14 @@
 
 from __future__ import absolute_import
 
-from ._app import (
-    validate_excel_sheet_name,
-    sanitize_excel_sheet_name
-)
+from ._app import sanitize_excel_sheet_name, validate_excel_sheet_name
 from ._error import (
-    NullNameError,
-    InvalidNameError,
-    InvalidCharError,
-    InvalidCharWindowsError,
-    InvalidLengthError,
-    ReservedNameError,
-    ValidReservedNameError,
-    InvalidReservedNameError,
-)
-from ._file import (
-    validate_filename,
-    validate_file_path,
-    sanitize_filename,
-    sanitize_file_path,
-)
-from ._ltsv import (
-    validate_ltsv_label,
-    sanitize_ltsv_label,
-)
-from ._sqlite import (
-    validate_sqlite_table_name,
-    validate_sqlite_attr_name,
-)
-from ._symbol import (
-    validate_symbol,
-    replace_symbol,
-)
+    InvalidCharError, InvalidCharWindowsError, InvalidLengthError, InvalidNameError,
+    InvalidReservedNameError, NullNameError, ReservedNameError, ValidReservedNameError)
+from ._file import sanitize_file_path, sanitize_filename, validate_file_path, validate_filename
+from ._ltsv import sanitize_ltsv_label, validate_ltsv_label
+from ._sqlite import validate_sqlite_attr_name, validate_sqlite_table_name
+from ._symbol import replace_symbol, validate_symbol
 from .variable import (
-    validate_js_var_name,
-    sanitize_js_var_name,
-    validate_python_var_name,
-    sanitize_python_var_name,
-    ElasticsearchIndexNameSanitizer,
-    JavaScriptVarNameSanitizer,
-    PythonVarNameSanitizer,
-)
+    ElasticsearchIndexNameSanitizer, JavaScriptVarNameSanitizer, PythonVarNameSanitizer,
+    sanitize_js_var_name, sanitize_python_var_name, validate_js_var_name, validate_python_var_name)
