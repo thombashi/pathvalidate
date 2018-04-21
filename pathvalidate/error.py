80debe08f3982c2afb66d856ca24629ee38fc41e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-04-21 23:19:56 +0900

    Add additional information to error classes

--- pathvalidate/error.py
+++ pathvalidate/error.py
@@ -6,11 +6,35 @@
 
 from __future__ import absolute_import, unicode_literals
 
+import enum
+
+
+@enum.unique
+class ErrorReason(enum.Enum):
+    NULL_NAME = "NULL_NAME"
+    INVALID_CHARACTER = "INVALID_CHARACTER"
+    INVALID_LENGTH = "INVALID_LENGTH"
+    RESERVED_NAME = "RESERVED_NAME"
+
 
 class ValidationError(ValueError):
     """
     Base exception class that indicates invalid name errors.
     """
+
+    @property
+    def reason(self):
+        return self.__reason
+
+    @property
+    def description(self):
+        return self.__description
+
+    def __init__(self, *args, **kwargs):
+        self.__reason = kwargs.pop("reason", None)
+        self.__description = kwargs.pop("description", None)
+
+        super(ValidationError, self).__init__(*args, **kwargs)
 
 
 class NullNameError(ValidationError):
@@ -18,11 +42,21 @@ class NullNameError(ValidationError):
     Exception raised when a name is empty.
     """
 
+    def __init__(self, *args, **kwargs):
+        kwargs["reason"] = ErrorReason.NULL_NAME
+
+        super(NullNameError, self).__init__(args, kwargs)
+
 
 class InvalidCharError(ValidationError):
     """
     Exception raised when includes invalid character(s) within a string.
     """
+
+    def __init__(self, *args, **kwargs):
+        kwargs["reason"] = ErrorReason.INVALID_CHARACTER
+
+        super(InvalidCharError, self).__init__(args, kwargs)
 
 
 class InvalidCharWindowsError(InvalidCharError):
@@ -37,11 +71,21 @@ class InvalidLengthError(ValidationError):
     Exception raised when a string too long/short.
     """
 
+    def __init__(self, *args, **kwargs):
+        kwargs["reason"] = ErrorReason.INVALID_LENGTH
+
+        super(InvalidLengthError, self).__init__(args, kwargs)
+
 
 class ReservedNameError(ValidationError):
     """
     Exception raised when a string matched a reserved name.
     """
+
+    def __init__(self, *args, **kwargs):
+        kwargs["reason"] = ErrorReason.RESERVED_NAME
+
+        super(ReservedNameError, self).__init__(args, kwargs)
 
 
 class ValidReservedNameError(ReservedNameError):
--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ import sys
 
 import setuptools
 
+
 MODULE_NAME = "pathvalidate"
 REPOSITORY_URL = "https://github.com/thombashi/{:s}".format(MODULE_NAME)
 REQUIREMENT_DIR = "requirements"
@@ -51,6 +52,7 @@ with open(os.path.join(REQUIREMENT_DIR, "test_requirements.txt")) as f:
 with open(os.path.join(REQUIREMENT_DIR, "docs_requirements.txt")) as f:
     DOCS_REQUIRES = [line.strip() for line in f if line.strip()]
 
+SETUPTOOLS_REQUIRES = ["setuptools>=38.3.0"]
 PYTEST_RUNNER_REQUIRES = ["pytest-runner"] if need_pytest() else []
 cmdclass = {}
 cmdclass.update(get_release_command_class())
@@ -70,14 +72,15 @@ setuptools.setup(
     license=pkg_info["__license__"],
     long_description=long_description,
     include_package_data=True,
-    install_requires=install_requires,
+
     packages=setuptools.find_packages(exclude=["test*"]),
     project_urls={
         "Documentation": "http://{typepy}.rtfd.io/",
         "Tracker": "{:s}/issues".format(REPOSITORY_URL),
     },
 
-    setup_requires=PYTEST_RUNNER_REQUIRES,
+    install_requires=SETUPTOOLS_REQUIRES + install_requires,
+    setup_requires=SETUPTOOLS_REQUIRES + PYTEST_RUNNER_REQUIRES,
     tests_require=tests_require,
     extras_require={
         "build": "wheel",
