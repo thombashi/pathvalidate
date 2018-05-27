aef12168003db3aeefb1dd869bb318989c5c135a
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2018-05-27 18:37:34 +0900

    Update docs

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-import pkg_resources
+from pathvalidate import __author__, __copyright__, __name__, __version__
 import sphinx_rtd_theme
 
 
@@ -43,16 +43,16 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'pathvalidate'
-copyright = u'2016, Tsuyoshi Hombashi'
-author = u'Tsuyoshi Hombashi'
+project = __name__
+copyright = __copyright__
+author = __author__
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = pkg_resources.get_distribution("pathvalidate").version
+version = __version__
 # The full version, including alpha/beta/rc tags.
 release = version
 
@@ -223,7 +223,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'pathvalidate.tex', u'pathvalidate Documentation',
-     u'Tsuyoshi Hombashi', 'manual'),
+     __author__, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
