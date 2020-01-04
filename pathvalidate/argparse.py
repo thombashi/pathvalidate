d66aa60dc152c4bc549a4b6addab35427cdf1654
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 15:29:13 +0900

    Add validate/sanitize functions for argparse

--- pathvalidate/argparse.py
+++ pathvalidate/argparse.py
@@ -10,7 +10,30 @@ from ._file import sanitize_filename, sanitize_filepath, validate_filename, vali
 from .error import ValidationError
 
 
+def validate_filename_arg(value: str) -> None:
+    try:
+        validate_filename(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+
+def validate_filepath_arg(value: str) -> None:
+    try:
+        validate_filepath(value)
+    except ValidationError as e:
+        raise ArgumentTypeError(e)
+
+
+def sanitize_filename_arg(value: str) -> PathType:
+    return sanitize_filename(value)
+
+
+def sanitize_filepath_arg(value: str) -> PathType:
+    return sanitize_filepath(value)
+
+
 def filename(value: PathType) -> PathType:
+    # Deprecated
     try:
         validate_filename(value)
     except ValidationError as e:
@@ -20,6 +43,7 @@ def filename(value: PathType) -> PathType:
 
 
 def filepath(value: PathType) -> PathType:
+    # Deprecated
     try:
         validate_filepath(value)
     except ValidationError as e:
--- test/test_argparse.py
+++ test/test_argparse.py
@@ -1,23 +1,26 @@
-from argparse import ArgumentError, ArgumentParser, ArgumentTypeError
+from argparse import ArgumentError, ArgumentParser
 
 import pytest
 
-from pathvalidate.argparse import filename, filepath
+from pathvalidate.argparse import (
+    sanitize_filename_arg,
+    sanitize_filepath_arg,
+    validate_filename_arg,
+    validate_filepath_arg,
+)
 
 
-class Test_argparse_filename_validator(object):
+class Test_validate_filename_arg:
     @pytest.mark.parametrize(["value"], [["abc"], ["abc.txt"]])
     def test_normal(self, value):
         parser = ArgumentParser()
-        parser.add_argument("filename", type=filename)
-
-        assert parser.parse_args([value])
-        assert filename(value)
+        parser.add_argument("filename", type=validate_filename_arg)
+        parser.parse_args([value])
 
     @pytest.mark.parametrize(["value"], [["foo/abc"], ["a?c"], ["COM1"], ["a" * 8000]])
     def test_exception(self, value):
         parser = ArgumentParser()
-        parser.add_argument("filename", type=filename)
+        parser.add_argument("filename", type=validate_filename_arg)
 
         try:
             parser.parse_args([value])
@@ -26,23 +29,18 @@ class Test_argparse_filename_validator(object):
         else:
             raise RuntimeError()
 
-        with pytest.raises(ArgumentTypeError):
-            filename(value)
 
-
-class Test_argparse_filepath_validator(object):
+class Test_validate_filepath_arg:
     @pytest.mark.parametrize(["value"], [["foo/abc"], ["foo/abc.txt"]])
     def test_normal(self, value):
         parser = ArgumentParser()
-        parser.add_argument("filepath", type=filepath)
-
-        assert parser.parse_args([value])
-        assert filepath(value)
+        parser.add_argument("filepath", type=validate_filepath_arg)
+        parser.parse_args([value])
 
     @pytest.mark.parametrize(["value"], [["foo/a?c"], ["COM1"], ["a" * 8000]])
     def test_exception(self, value):
         parser = ArgumentParser()
-        parser.add_argument("filepath", type=filepath)
+        parser.add_argument("filepath", type=validate_filepath_arg)
 
         try:
             parser.parse_args([value])
@@ -51,5 +49,36 @@ class Test_argparse_filepath_validator(object):
         else:
             raise RuntimeError()
 
-        with pytest.raises(ArgumentTypeError):
-            filepath(value)
+
+class Test_sanitize_filename_arg:
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            ["abc", "abc"],
+            ["abc.txt", "abc.txt"],
+            ["foo/abc", "fooabc"],
+            ["a?c", "ac"],
+            ["COM1", "COM1_"],
+        ],
+    )
+    def test_normal(self, value, expected):
+        parser = ArgumentParser()
+        parser.add_argument("filename", type=sanitize_filename_arg)
+        assert parser.parse_args([value]).filename == expected
+
+
+class Test_sanitize_filepath_arg:
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            ["abc", "abc"],
+            ["abc.txt", "abc.txt"],
+            ["foo/abc", "foo/abc"],
+            ["a?c", "ac"],
+            ["COM1", "COM1_"],
+        ],
+    )
+    def test_normal(self, value, expected):
+        parser = ArgumentParser()
+        parser.add_argument("filepath", type=sanitize_filepath_arg)
+        assert parser.parse_args([value]).filepath == expected
