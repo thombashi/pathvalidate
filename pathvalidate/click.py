b825ca45d93d16dbd4833022dda9d36a9e77e8d1
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 15:49:21 +0900

    Add validate/sanitize functions for click

--- pathvalidate/click.py
+++ pathvalidate/click.py
@@ -5,11 +5,47 @@
 
 import click
 
+from ._common import PathType
 from ._file import sanitize_filename, sanitize_filepath, validate_filename, validate_filepath
 from .error import ValidationError
 
 
+def validate_filename_arg(ctx, param, value) -> None:
+    if not value:
+        return
+
+    try:
+        validate_filename(value)
+    except ValidationError as e:
+        raise click.BadParameter(str(e))
+
+
+def validate_filepath_arg(ctx, param, value) -> None:
+    if not value:
+        return
+
+    try:
+        validate_filepath(value)
+    except ValidationError as e:
+        raise click.BadParameter(str(e))
+
+
+def sanitize_filename_arg(ctx, param, value) -> PathType:
+    if not value:
+        return ""
+
+    return sanitize_filename(value)
+
+
+def sanitize_filepath_arg(ctx, param, value) -> PathType:
+    if not value:
+        return ""
+
+    return sanitize_filepath(value)
+
+
 def filename(ctx, param, value):
+    # Deprecated
     if not value:
         return None
 
@@ -22,6 +58,7 @@ def filename(ctx, param, value):
 
 
 def filepath(ctx, param, value):
+    # Deprecated
     if not value:
         return None
 
