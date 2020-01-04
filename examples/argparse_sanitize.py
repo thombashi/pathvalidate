921b1098e6b64f811240a7d75f1f6b08084ce988
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-01-04 20:31:33 +0900

    Update examples

--- examples/argparse_sanitize.py
+++ examples/argparse_sanitize.py
@@ -3,12 +3,12 @@
 
 from argparse import ArgumentParser
 
-from pathvalidate.argparse import validate_filename_arg, validate_filepath_arg
+from pathvalidate.argparse import sanitize_filename_arg, sanitize_filepath_arg
 
 
 parser = ArgumentParser()
-parser.add_argument("--filename", type=validate_filename_arg)
-parser.add_argument("--filepath", type=validate_filepath_arg)
+parser.add_argument("--filename", type=sanitize_filename_arg)
+parser.add_argument("--filepath", type=sanitize_filepath_arg)
 options = parser.parse_args()
 
 if options.filename:
--- examples/click_sanitize.py
+++ examples/click_sanitize.py
@@ -2,12 +2,12 @@
 
 import click
 
-from pathvalidate.click import validate_filename_arg, validate_filepath_arg
+from pathvalidate.click import sanitize_filename_arg, sanitize_filepath_arg
 
 
 @click.command()
-@click.option("--filename", callback=validate_filename_arg)
-@click.option("--filepath", callback=validate_filepath_arg)
+@click.option("--filename", callback=sanitize_filename_arg)
+@click.option("--filepath", callback=sanitize_filepath_arg)
 def cli(filename, filepath):
     if filename:
         click.echo("filename: {}".format(filename))
