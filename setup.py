efc71a0fb0d37fb9c64f7d01fa331de4937d0f34
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2020-03-20 20:03:45 +0900

    Replace literal with a constant

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setuptools.setup(
     long_description_content_type="text/x-rst",
     include_package_data=True,
     packages=setuptools.find_packages(exclude=["test*"]),
-    package_data={"pathvalidate": ["py.typed"]},
+    package_data={MODULE_NAME: ["py.typed"]},
     project_urls={
         "Documentation": "https://{:s}.rtfd.io/".format(MODULE_NAME),
         "Source": REPOSITORY_URL,
