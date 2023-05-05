e5115a13aea74f45bd9bb2a993ef4d32e9aabd8e
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-05-05 19:53:41 +0900

    Apply formatter

--- test/test_filename.py
+++ test/test_filename.py
@@ -584,7 +584,13 @@ class Test_sanitize_filename:
         assert filename == expected
         assert is_valid_filename(filename, platform=platform)
 
-    @pytest.mark.parametrize(["value", "expected"], [[1, TypeError], [True, TypeError]])
+    @pytest.mark.parametrize(
+        ["value", "expected"],
+        [
+            [1, TypeError],
+            [True, TypeError],
+        ],
+    )
     def test_exception_type(self, value, expected):
         with pytest.raises(expected):
             sanitize_filename(value)
