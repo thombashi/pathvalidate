c39465ac6c9cdbd9baa9b45d71b7ab5413862c89
Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
2023-08-12 00:46:22 +0900

    Update docstrings

--- pathvalidate/handler.py
+++ pathvalidate/handler.py
@@ -13,12 +13,39 @@ NullValueHandler = Callable[[ValidationError], str]
 
 
 def return_null_string(e: ValidationError) -> str:
+    """Null value handler that always returns an empty string.
+
+    Args:
+        e (ValidationError): A validation error.
+
+    Returns:
+        str: An empty string.
+    """
+
     return ""
 
 
 def return_timestamp(e: ValidationError) -> str:
+    """Null value handler that returns a timestamp of when the function was called.
+
+    Args:
+        e (ValidationError): A validation error.
+
+    Returns:
+        str: A timestamp.
+    """
+
     return str(datetime.now().timestamp())
 
 
 def raise_error(e: ValidationError) -> str:
+    """Null value handler that always raises an exception.
+
+    Args:
+        e (ValidationError): A validation error.
+
+    Raises:
+        ValidationError: Always raised.
+    """
+
     raise e
